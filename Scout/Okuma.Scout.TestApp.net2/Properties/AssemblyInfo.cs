934e8f9e6ab472a7a4fb5cc23c28fb897b17465b
Scott Solmer <okuma.scott@gmail.com>
2019-01-22 08:35:22 -0500

    Updated SCOUT items

--- Scout/Okuma.Scout.TestApp.net2/Properties/AssemblyInfo.cs
+++ Scout/Okuma.Scout.TestApp.net2/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Okuma America Corporation")]
 [assembly: AssemblyProduct("Okuma.Scout.TestApp.net2")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("© Okuma 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.12.0.0")]
-[assembly: AssemblyFileVersion("2.12.0.0")]
+[assembly: AssemblyVersion("2.12.37.0")]
+[assembly: AssemblyFileVersion("2.12.37.0")]
